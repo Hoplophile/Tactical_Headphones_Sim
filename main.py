14424f194546fb0edaa7a6799be6fb0852e50178
Piotr Ziebinski <piotr.ziebinski@aptiv.com>
2019-11-06 14:21:47 +0100

    moved sounds to /wav, plotting 4 signals at once

--- main.py
+++ main.py
@@ -6,16 +6,18 @@ from plotting import *
 from scipy.fftpack import fft
 
 
-path_to_wav = 'C:/Users/zj6pz3/PycharmProjects/Tactical_headphones/gunfire.wav'
-path_to_wav2 = 'C:/Users/zj6pz3/PycharmProjects/Tactical_headphones/gunfire2.wav'
-order = 5
-cutoff = 20000      # desired cutoff frequency of the filter, Hz
-sr = 705000.0       # sample rate in Hz
+path_to_wav = 'C:/Users/zj6pz3/PycharmProjects/Tactical_headphones/wavs/M4A1.wav'
+path_to_wav2 = 'C:/Users/zj6pz3/PycharmProjects/Tactical_headphones/wavs/beretta_m9.wav'
+path_to_wav3 = 'C:/Users/zj6pz3/PycharmProjects/Tactical_headphones/wavs/AK47.wav'
+path_to_wav4 = 'C:/Users/zj6pz3/PycharmProjects/Tactical_headphones/wavs/mossberg_500.wav'
+order = 1
+cutoff = 10000      # desired cutoff frequency of the filter, Hz
+sr = 512000.0       # sample rate in Hz
 
 
 def gen_sine():
     rate = sr
-    freq = 800
+    freq = 100
     x = np.arange(80000)
 
     sinus = np.sin(2 * np.pi * freq * x / rate)
@@ -26,25 +28,65 @@ def main():
     # Get signal from .wav file
     wav_signal, wav_time = get_signal_from_file(path_to_wav)
     wav2_signal, wav2_time = get_signal_from_file(path_to_wav2)
-    sin_signal, sin_time = gen_sine()  # test sine
+    wav3_signal, wav3_time = get_signal_from_file(path_to_wav3)
+    wav4_signal, wav4_time = get_signal_from_file(path_to_wav4)
+    sin_signal, sin_time = gen_sine()
 
     # Filter the data
     wav_filtered = butter_lowpass_filter(wav_signal, cutoff, sr, order)
     wav2_filtered = butter_lowpass_filter(wav2_signal, cutoff, sr, order)
-    sin_filtered = butter_lowpass_filter(sin_signal, cutoff, sr, order)
+    wav3_filtered = butter_lowpass_filter(wav3_signal, cutoff, sr, order)
+    wav4_filtered = butter_lowpass_filter(wav4_signal, cutoff, sr, order)
 
     # Fourier transform
     fourier_signal = fft(wav_filtered)
-    fourier_signal = fourier_signal[:len(fourier_signal)//2]
-    fourier_x = np.linspace(0.0, sr/2, len(fourier_signal))
+    # fourier_signal = fourier_signal[:len(fourier_signal)//2]
+    fourier_x = np.linspace(0.0, stop=sr, num=len(fourier_signal))
+    fourier_x = fourier_x[:len(fourier_signal)]
 
-    # Plot data
-    # plot_two_sep(wav_signal, 'original', wav_time, wav_filtered, 'filtered', wav_time)
-    # plot_one(wav_filtered, 'filtered', wav_time)
-    plot_two_sep(wav_filtered, 'filtered signal', wav_time,
-                 np.abs(fourier_signal), 'fft', fourier_x)
-    # plot_one(np.abs(fourier[:len(fourier)//100]), 'fourier', np.linspace(0.0, sr, 20912//100))
-    # plot_one(wav_filtered, 'sinus', wav_time)
+    fourier_signal2 = fft(wav2_filtered)
+    fourier_signal2 = fourier_signal2[:len(fourier_signal2)//2]
+    fourier_x2 = np.linspace(0.0, sr/2, len(fourier_signal2))
+
+    fourier_signal3 = fft(wav3_filtered)
+    fourier_signal3 = fourier_signal3[:len(fourier_signal3)//2]
+    fourier_x3 = np.linspace(0.0, sr/2, len(fourier_signal3))
+
+    fourier_signal4 = fft(wav4_filtered)
+    fourier_signal4 = fourier_signal4[:len(fourier_signal4)//2]
+    fourier_x4 = np.linspace(0.0, sr/2, len(fourier_signal4))
+
+    fourier_signal_sin = fft(sin_signal)
+    fourier_signal_sin = fourier_signal_sin[:len(fourier_signal_sin)//2]
+    fourier_x_sin = np.linspace(0.0, sr/2, len(fourier_signal_sin))
+
+    # Plot all orig+filtered
+    plot_four_with_filt_sep(wav_signal, wav_filtered, wav_time, 'M4A1',
+                            wav2_signal, wav2_filtered, wav2_time, 'Beretta M9',
+                            wav3_signal, wav3_filtered, wav3_time, 'AK47',
+                            wav4_signal, wav4_filtered, wav4_time, 'Mossberg 500')
+
+    # Plot M4 with fft
+    plot_orig_filt_fft(wav_signal, 'M4A1', wav_time,                                # original
+                       wav_filtered, 'filtered M4A1', wav_time,                     # filtered
+                       np.abs(fourier_signal), 'FFT of filtered M4A1', fourier_x)   # fft
+
+    # Plot M9 with fft
+    plot_orig_filt_fft(wav2_signal, 'Beretta', wav2_time,                                # original
+                       wav2_filtered, 'filtered Beretta', wav2_time,                     # filtered
+                       np.abs(fourier_signal2), 'FFT of filtered Beretta', fourier_x2)   # fft
+
+    # Plot AK47 with fft
+    plot_orig_filt_fft(wav3_signal, 'AK47', wav3_time,                                # original
+                       wav3_filtered, 'filtered AK47', wav3_time,                     # filtered
+                       np.abs(fourier_signal3), 'FFT of filtered AK47', fourier_x3)   # fft
+
+    # Plot Mossberg with fft
+    plot_orig_filt_fft(wav4_signal, 'Mossberg', wav4_time,                                # original
+                       wav4_filtered, 'filtered Mossberg', wav4_time,                     # filtered
+                       np.abs(fourier_signal4), 'FFT of filtered Mossberg', fourier_x4)   # fft
+
+    plt.show()
 
 
 if __name__ == '__main__':
--- src/plotting.py
+++ src/plotting.py
@@ -9,7 +9,7 @@ def plot_two_sep(first, caption_first, first_t, second, caption_second, second_t
     plt.subplot(2, 1, 1)
     plt.plot(first_t, first, 'b-', linewidth=0.5, label=caption_first)
     plt.legend()
-    plt.xlim(right=0.2, left=-0.01)
+    plt.xlim(right=0.15, left=-0.01)
     plt.grid()
     plt.subplot(2, 1, 2)
     plt.plot(second_t, second, 'g-', linewidth=0.5, label=caption_second, scalex=0.1)
@@ -19,6 +19,64 @@ def plot_two_sep(first, caption_first, first_t, second, caption_second, second_t
 
     plt.subplots_adjust(hspace=0.35)
     plt.show()
+
+
+def plot_orig_filt_fft(orig, orig_txt, orig_t, filt, filt_txt, filt_t, fft, fft_txt, fft_t):
+    plt.figure(figsize=(17, 8))
+    plt.subplot(2, 1, 1)
+    plt.plot(orig_t, orig, 'b-', linewidth=0.5, label=orig_txt)
+    plt.plot(filt_t, filt, 'r-', linewidth=2, label=filt_txt)
+    plt.xlabel('Time [sec]')
+    plt.legend()
+    # plt.xlim(right=0.2, left=-0.01)
+    plt.grid()
+    plt.subplot(2, 1, 2)
+    plt.plot(fft_t, fft, 'g-', linewidth=1.5, label=fft_txt)
+    plt.xlabel('Frequency [Hz]')
+    plt.legend()
+    plt.xlim(right=20000, left=-0.01)
+    plt.grid()
+
+    plt.subplots_adjust(hspace=0.35)
+    # plt.show()
+
+
+def plot_four_with_filt_sep(sig1, filt1, time1, name1,
+                            sig2, filt2, time2, name2,
+                            sig3, filt3, time3, name3,
+                            sig4, filt4, time4, name4):
+    plt.figure(figsize=(17, 8))
+    plt.subplot(2, 2, 1)
+    plt.plot(time1, sig1, 'b-', linewidth=0.5, label=name1)
+    plt.plot(time1, filt1, 'r-', linewidth=2, label=(name1 + ' filtered'))
+    plt.xlabel('Time [sec]')
+    plt.legend()
+    # plt.xlim(right=0.15, left=-0.01)
+    plt.grid()
+    plt.subplot(2, 2, 2)
+    plt.plot(time2, sig2, 'b-', linewidth=0.5, label=name2)
+    plt.plot(time2, filt2, 'r-', linewidth=2, label=(name2 + ' filtered'))
+    plt.xlabel('Time [sec]')
+    plt.legend()
+    # plt.xlim(right=0.15, left=-0.01)
+    plt.grid()
+    plt.subplot(2, 2, 3)
+    plt.plot(time3, sig3, 'b-', linewidth=0.5, label=name3)
+    plt.plot(time3, filt3, 'r-', linewidth=2, label=(name3 + ' filtered'))
+    plt.xlabel('Time [sec]')
+    plt.legend()
+    # plt.xlim(right=0.15, left=-0.01)
+    plt.grid()
+    plt.subplot(2, 2, 4)
+    plt.plot(time4, sig4, 'b-', linewidth=0.5, label=name4)
+    plt.plot(time4, filt4, 'r-', linewidth=2, label=(name4 + ' filtered'))
+    plt.xlabel('Time [sec]')
+    plt.legend()
+    # plt.xlim(right=0.15, left=-0.01)
+    plt.grid()
+
+    plt.subplots_adjust(hspace=0.35)
+    # plt.show()
 
 
 def plot_two_on_one(first, caption_first, first_t, second, caption_second, second_t):
